ebd8abff18cdf92ada9e83f179b92a120c0703fd
Adil Abde <adilabde1994@gmail.com>
2024-02-19 00:01:32 +0100

    Project had been created without Style and used more only of react-router @v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import Home from "./ui/Home"
+import Menu, { menuLoader } from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import CreateOrder, { action as CreateOrderAction } from "./features/order/CreateOrder"
+import Order, { OrderLoader } from "./features/order/Order"
+import AppLayout from "./ui/AppLayout"
+import Error from "./ui/Error"
+
+
+const router=createBrowserRouter([
+
+  {
+   element: <AppLayout />,
+   errorElement:<Error/>,
+   children:[
+    {
+      path:"/",
+      element:<Home />
+    },
+    {
+      path:"/menu",
+      element:<Menu />,
+      loader: menuLoader,
+      errorElement:<Error />
+    },
+    {
+      path:"/cart",
+      element:<Cart />
+    },
+    {
+      path:"/order/new",
+      element:<CreateOrder />,
+      action:CreateOrderAction
+    },
+    {
+      path:"order/:orderId",
+      element:<Order />,
+      loader:OrderLoader,
+      errorElement:<Error />
+
+
+    }
+   ]
+
+  }
+  
+])
+
+
+export default function App() {
+  
+ return <RouterProvider router={router} />
 }
-
-export default App
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loading from "./Loading";
+
+export default function AppLayout() {
+   const navigation = useNavigation();
+   const isLoading = navigation.state === "loading";
+
+   return (
+      <div className="layout">
+         {isLoading && <Loading />}
+
+         <Header />
+
+
+         <main>
+            <Outlet />
+         </main>
+
+         <CartOverview />
+      </div>
+   );
+}
